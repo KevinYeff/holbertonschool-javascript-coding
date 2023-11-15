d96a1274d024491d2f3226b6025c3ee8d2224cbe
KevinYeff <kevinyeff94@gmail.com>
2023-11-15 09:34:42 -0500

    feat: Task 5 Loripsum Script that gets the contents of a webpage and stores it a file

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+const URL = process.argv[2];
+const fileToWrite = process.argv[3];
+const fs = require('fs');
+
+request.get(URL, (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  fs.writeFile(fileToWrite, body, 'utf-8', (err) => {
+    if (err) throw err;
+  })
+});

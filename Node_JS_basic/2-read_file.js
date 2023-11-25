681137fece9c45d25580556818389918f9229a0d
KevinYeff <kevinyeff94@gmail.com>
2023-11-25 12:39:16 -0500

    fix: filtering empty lines at the end

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,3 +1,4 @@
+const { resolveObjectURL } = require('buffer');
 const fs = require('fs');
 
 module.exports = function countStudents(path) {
@@ -12,10 +13,13 @@ module.exports = function countStudents(path) {
   const parsedFields = [...new Set(fields)];
 
   for (const field of parsedFields) {
-    const student = noTitleData.filter((row) => row.endsWith(field)).map((row) => {
-      const splitedLine = row.split(',');
-      return splitedLine[0];
-    });
+    const student = noTitleData
+      .filter((row) => row.trim())
+      .filter((row) => row.endsWith(field))
+      .map((row) => {
+        const splitedLine = row.split(',');
+        return splitedLine[0];
+      });
     console.log(`Number of students in ${field}: ${student.length}. List: ${student.join(', ')}`);
   }
 };

901eff4b6a5bbfade50fb4d13867a8635f57d6d1
KevinYeff <kevinyeff94@gmail.com>
2023-11-29 00:44:37 -0500

    feat: Task 5 Create a More Complex HTTP server using Node's HTTP module

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,38 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const countStudents = require('./3-read_file_async');
+
+const dataBaseFile = process.argv.length > 2 ? process.argv[2] : '';
+
+const app = http.createServer((req, res) => {
+  const { url } = req;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(dataBaseFile)
+      .then((result) => {
+        res.write(`Number of students ${result.totalStudents}\n`);
+        // console.log(result.fields);
+        result.fields.forEach(({ field, count, list }) => {
+          res.write(`Number of students in ${field}: ${count}. List: ${list}`);
+        });
+        res.end();
+      })
+      .catch((error) => {
+        res.statusCode = 500;
+        res.end(error.message);
+      });
+  }
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
+
+module.exports = app;
